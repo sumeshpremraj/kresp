5535d767570f4a828020000d3fabd90ea861676c
Sumesh P <sumeshpremraj@gmail.com>
2016-01-07 01:55:40 +0530

    Generate pdf from rss

--- crawler.py
+++ crawler.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python
 import feedparser
 import pdfkit
+content = '<html>'
 d = feedparser.parse('http://katekendall.com/feed/')
+for i in d.entries:
+    content += '<h3><a href="' + i['link'] + '>' + i['title'] + '</a></h3>\n'
+    content += '<p>' + i['description'] + '</p>\n'
+content += '</html>'
+#print content
+
+pdfkit.from_string(content, 'out.pdf')
+
 #pdfkit.from_string("<html> %s" % d.entries[0]['description'] + "</html>", 'out.pdf')
 #pdfkit.from_string("<html> hello </html>", 'out.pdf')
-pdfkit.from_url('http://google.com', 'out.pdf')
-print d.entries[0]['title']
-print d.entries[0]['description']
+#pdfkit.from_url('http://google.com', 'out.pdf')
