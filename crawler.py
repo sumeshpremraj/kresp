33e035a63f3d44114b5c73131318aa700be554b7
Sumesh P <sumeshpremraj@gmail.com>
2016-01-07 02:22:19 +0530

    Fix PDF render

--- crawler.py
+++ crawler.py
@@ -1,15 +1,21 @@
 #!/usr/bin/env python
 import feedparser
 import pdfkit
+import jjjjj
+
 content = '<html>'
 d = feedparser.parse('http://katekendall.com/feed/')
 for i in d.entries:
-    content += '<h3><a href="' + i['link'] + '>' + i['title'] + '</a></h3>\n'
-    content += '<p>' + i['description'] + '</p>\n'
+    content += '<h3><a href="' + i['link'] + '">' + i['title'] + '</a></h3>\n'
+    content += '<p>' + i['content'][0]['value'] + '</p>\n'
+    #print i['content'][0]['value']
 content += '</html>'
 #print content
+pdfkit.from_string(content, 'newsletter.pdf')
 
-pdfkit.from_string(content, 'out.pdf')
+
+
+
 
 #pdfkit.from_string("<html> %s" % d.entries[0]['description'] + "</html>", 'out.pdf')
 #pdfkit.from_string("<html> hello </html>", 'out.pdf')
