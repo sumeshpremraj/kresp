dbcf88ef513bd6db4dd3b53255a84ae88ff59759
Satya Brat Singh <satya.singh@flipkart.com>
2016-01-07 01:29:37 +0530

    modified crawler -  html to pdf

--- crawler.py
+++ crawler.py
@@ -1,4 +1,9 @@
-#!/usr/bin/python
+#!/usr/bin/env python
+import feedparser
+import pdfkit
 d = feedparser.parse('http://katekendall.com/feed/')
+#pdfkit.from_string("<html> %s" % d.entries[0]['description'] + "</html>", 'out.pdf')
+#pdfkit.from_string("<html> hello </html>", 'out.pdf')
+pdfkit.from_url('http://google.com', 'out.pdf')
 print d.entries[0]['title']
 print d.entries[0]['description']
